a444f085b22d0c3e26fba61af546ef4a0fee60d8
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2017-02-05 04:14:29 +0900

    initial commit

---
+++ src/main/java/twitter/clone/GreetingController.java
@@ -0,0 +1,18 @@
+package twitter.clone;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
